08624373c6924caf9d7856f1bd58c42cb98dc87e
Samvit Jatia <samvit@hotmail.com>
2021-11-07 18:34:15 +0530

    register page code not working

--- frontend/src/register.js
+++ frontend/src/register.js
@@ -1,61 +1,135 @@
 // needs back-end work to be functional
 
-import React from "react";
+import React, { Component } from "react";
 import "./register.css";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import image from "./photos/Artkart-logo.png";
+import axios from "axios";
 
-function Register() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
-    const [address, setAddress] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      email: "",
+      mobile: "",
+      address: "",
+      password: "",
+    };
+    this.changeusername = this.changeusername.bind(this);
+    this.changeemail = this.changeemail.bind(this);
+    this.changemobile = this.changemobile.bind(this);
+    this.changeaddress = this.changeaddress.bind(this);
+    this.changepassword = this.changepassword.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  // const register = (e) => {
+  //   e.preventDefault();
+  //   // back-end work req. Not sure about this line
+  // };
 
-    const register = e => {
-        e.preventDefault();
-        // back-end work req. Not sure about this line 
-      }
-    return(
-        <div className="register">
-      <Link to="/">
-        <img className="register_logo" src={image} />
-      </Link>
+  changeusername(event) {
+    this.setState({
+      username: event.target.value,
+    });
+  }
+  changeemail(event) {
+    this.setState({
+      email: event.target.value,
+    });
+  }
+  changemobile(event) {
+    this.setState({
+      mobile: event.target.value,
+    });
+  }
+  changeaddress(event) {
+    this.setState({
+      address: event.target.value,
+    });
+  }
+  changepassword(event) {
+    this.setState({
+      password: event.target.value,
+    });
+  }
 
-    <div className="register_container">
-        <h1>Create New Account</h1>
-        <form>
-          <h5>Your Name</h5>
-          <input type="text" value={name} onChange={e => setName(e.target.value)} />
+  onSubmit(event) {
+    event.preventDefualt();
 
-          <h5>Your E-mail</h5>
-          <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
+    const registered = {
+      username: this.state.username,
+      email: this.state.email,
+      mobile: this.state.mobile,
+      address: this.state.address,
+      password: this.state.password,
+    };
+    console.log("working");
+    axios
+      .post("http://127.0.0.1:4000/app/signup", registered)
+      .then((response) => console.log(response.data));
 
-          <h5>Your Mobile Number</h5>
-          <input type="text" value={phone} onChange={e => setPhone(e.target.value)}/>
+    window.location = "/";
+  }
 
-          <h5>Your Address</h5>
-          <input type="text" value={address} onChange={e => setAddress(e.target.value)}/>
+  render = () => {
+    return (
+      <div className="register">
+        <Link to="/">
+          <img className="register_logo" src={image} />
+        </Link>
 
-          <h5>Create Password</h5>
-          <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+        <div className="register_container">
+          <h1>Create New Account</h1>
+          <form onSubmit={this.onSubmit}>
+            <h5>Your Name</h5>
+            <input
+              type="text"
+              onChange={this.changeusername}
+              value={this.state.username}
+            />
+            <h5>Your E-mail</h5>
+            <input
+              type="text"
+              onChange={this.changeemail}
+              value={this.state.email}
+            />
 
-          <h5>Confirm Password</h5>
-          <input type="password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)}/>
+            <h5>Your Mobile Number</h5>
+            <input
+              type="text"
+              onChange={this.changemobile}
+              value={this.state.mobile}
+            />
 
-          <button type="submit"  onClick={register}  className="createaccount_button">
-            <strong>Create Account</strong>
-          </button>
-        </form>
-    </div>
-    </div>
-    )
+            <h5>Your Address</h5>
+            <input
+              type="text"
+              onChange={this.changeaddress}
+              value={this.state.address}
+            />
+
+            <h5>Create Password</h5>
+            <input
+              type="password"
+              onChange={this.changepassword}
+              value={this.state.password}
+            />
+
+            <button
+              type="submit"
+              value="Submit"
+              // onClick={register}
+              className="createaccount_button"
+            >
+              <strong>Create Account</strong>
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  };
 }
-
-
-
-
 
 export default Register;
